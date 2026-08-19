7e1fc471d5ac4a436908e95765f520d8a016d21f
rafaelprojetog2-rgb <rafaelcostadyautoparts-ops@users.noreply.github.com>
2026-08-19 16:11:31 -0300

    fix: padroniza barras superiores em producao

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dy-autoparts-v249';
+const CACHE_NAME = 'dy-autoparts-v252';
 
 // Pre-cache sem query strings; o match usa ignoreSearch para funcionar
 // independentemente da versao usada pelo index.html
