2f02469c4d2dcf3dbe36bc4e01eda83a9e4ca9c4
rafaelprojetog2-rgb <rafaelcostadyautoparts-ops@users.noreply.github.com>
2026-08-20 16:40:28 -0300

    Padroniza botao de retorno em todas as telas

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dy-autoparts-v259';
+const CACHE_NAME = 'dy-autoparts-v260';
 
 // Pre-cache sem query strings; o match usa ignoreSearch para funcionar
 // independentemente da versao usada pelo index.html
