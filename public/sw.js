2c81d4a52eb096db9a63b5d790312505ff0f6225
rafaelprojetog2-rgb <rafaelcostadyautoparts-ops@users.noreply.github.com>
2026-08-25 13:06:17 -0300

    Permite salvar Flex sem foto ou devolucao

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dy-autoparts-v276';
+const CACHE_NAME = 'dy-autoparts-v277';
 
 // Pre-cache sem query strings; o match usa ignoreSearch para funcionar
 // independentemente da versao usada pelo index.html
