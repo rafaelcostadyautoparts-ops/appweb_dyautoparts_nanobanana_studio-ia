b6afffbead5b88f46d0320e7faafb70d8c4ece12
rafaelprojetog2-rgb <rafaelcostadyautoparts-ops@users.noreply.github.com>
2026-08-25 13:13:10 -0300

    Centraliza e simplifica resumo mensal Flex

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dy-autoparts-v277';
+const CACHE_NAME = 'dy-autoparts-v278';
 
 // Pre-cache sem query strings; o match usa ignoreSearch para funcionar
 // independentemente da versao usada pelo index.html
